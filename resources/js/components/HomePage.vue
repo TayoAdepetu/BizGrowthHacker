6e8c232f3eb4ff4ca3ef558f5d84815b76e665ac
TayoAdepetu <copyafrica93@gmail.com>
2023-01-07 19:48:38 -0800

    first commit

---
+++ resources/js/components/HomePage.vue
@@ -0,0 +1,288 @@
+<template>
+    <div class="page-container">
+
+        <div class="top-area">
+            <div class="menu-bar menuu">
+            <div class="page-header">Business Growth Hacker</div>
+            <div class="menu-menu">
+                <div class="service-page">Services</div>
+                <div class="work-page-border">Work With Me</div>
+            </div>
+            </div>
+           
+            <div class="the-hero">
+                <div class="first-flex">
+                    <div class="hit-line">
+                        <div>I Want To Make You Some Money.</div>
+                        <div>Will You Permit Me?</div>
+                    </div>
+                    <div class="intro-line">
+                        <div class="name-tag">
+                        <span class="second-name">I am Tayo Adepetu</span><span class="first-name">, a Full-service Digital Marketer For Small and Medium-scale Businesses.</span>
+                        </div>
+                        
+                        <div>My aim is to help small and medium-scale businesses (just a few at a time) make more money by increasing their brand awareness, enhancing their online visibility, and communicating their offers clearly and persuasively.</div>
+                        <div class="brief-service">Copywriting, SEO, Socials, Emails, Branding, and Advertising.</div>
+                        <div class="call-to-action">Work With Me</div>
+                    </div>  
+                </div>          
+
+                <div class="my-image">
+                   <!--<img class="myself-back" src="/images/myself-image-background.png"></img> -->
+                    <img class="myself-img" src="/images/myself-image.png" >
+                </div>
+            </div>
+
+        </div>
+
+        <div class="service-opening">
+            <div class="money-hit-line">How Will I Help You Make <span class="more-money">More Money</span>?</div>
+            <div class="money-intro">
+                I believe the process of making money starts from adding value. 
+                Once you add value to your business and your target audience, it's only natural for your revenues to grow. 
+                So, here is how I will help add value to your business and audience.
+            </div>
+        </div>
+
+        <div class="service-section">
+            <div class="service-side">
+                <div>
+                <h3 class="service-title">Copywriting and Copy Optimization Services</h3>
+                <img class="service-image" src="/images/Tayo-Adepetu-Copywriting.jpg" alt="Tayo-Adepetu-Copywriting, image taken from https://www.vecteezy.com/free-vector/copywriting">
+                
+                <p class="service-tips">Generate More Leads + Increase Sales + Make More Money</p>
+                </div>
+
+                <div class="service-align">
+                    <p>
+                        Is your website not bringing enough sales or clients? Are your social media accounts not contributing enough to your revenues? 
+                        Are your ad copies disappoI Have The Solutions You Seek.
+                    </p>
+
+                    <p>
+                        Whether you’re offering products for sale or you’re in the services industry, my copies will sing the benefits of your 
+                        products/services to your target audience and turn them to your loyal customers. 
+                        My Search Engine Optimized Copies will locate both your existing and potential customers, and make them buy.
+                    </p>
+
+                    <p>
+                        Let me tell your brand’s story and share your business’ values with the world. 
+                        And lots of magic happens when I do that – enhanced brand awareness, increased engagement, brand consistency, more leads, more sales, and higher ROI.
+                    </p>
+
+                    <p>
+                        I believe Copywriting is a process. To write copies for you, I will follow what I call the “Copywriting Due Process” to ensure optimal success.
+                    </p>
+
+                    <p>
+                        Contact me to learn more about my “Copywriting Due Process” that will turn you into a winner.
+                    </p>
+                </div>
+            </div>
+            
+
+            <div class="service-side">
+                <div>
+                <h3 class="service-title">SEO and Content Marketing Services</h3>
+                <img class="service-image" src="/images/Tayo-Adepetu-SEO-Writing.jpg" alt="Tayo-Adepetu-SEO-Writing, image taken from https://www.vecteezy.com/free-vector/seo">
+                <p class="service-tips">Get Quality Content That Adds Value to Your Readers + Rank High in Search Engines</p>
+                </div>
+
+                <div class="service-align">
+                <P>
+                    Create website content that search engines love and let your target audience easily see your content through organic searches. 
+                    I can help showcase your Thought Leadership through educational, thought-provoking industry content.
+                </P>
+
+                <p>
+                    The content I’m offering you is plagiarism-free, error-free, search engine optimized, informative, and detailed. 
+                    My work will add value to your readers, and they will be grateful to you.
+                </P>
+
+                <p>
+                    From landing pages to company bios, product/category descriptions, adverts, headlines, newsletters, and other SEO and promotional contents, 
+                    content marketing strategies must be laser-focused on customer needs. Indeed, when it comes to content marketing and search engine optimization, 
+                    there's no substitute for quality content.
+                </p>
+
+                <p>
+                    With me on your side, your readers will keep coming back to read your articles. The more they read your articles, the more money you make.
+                </p>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="service-section">
+            <div class="service-side">
+                <div>
+                <h3 class="service-title">Email Marketing and Automation Services</h3>
+                <img class="service-image" src="/images/Tayo-Adepetu-Email-Marketing.jpg" alt="Tayo-Adepetu-Email-Marketing, image taken from https://www.vecteezy.com/free-vector/email-marketing">
+                <p class="service-tips">Reach New Audiences + Generate Leads + Get New Customers + Convince Old Customers To Buy and Turn Them To Your Brand's Advocates</p>
+
+                </div>
+
+                <div class="service-align">
+                <p>
+                    No doubt, Email marketing is an effective marketing strategy. In fact, it's one of the most effective marketing strategies out there. 
+                    However, for it to be so effective, you need to have a solid plan. 
+                    To have a solid Email marketing plan, you need to know your onions. Now, you don't have to take a course on Email marketing. I've done that for you.
+                     Just face other areas of your business and enjoy more time with your family. Being a Digital Marketer, Email marketing is my business.
+                </p>
+
+                <p>I can help you find the right audience, collect their data, segment the audience according to their buying journeys, and target each segment with relevant, 
+                    contextual emails that can add value to them and nurture them until they're able and ready to buy.
+                </p>  
+
+                <p>I have the tools and skills to help you generate new leads and convince both your new and existing customers to buy more from you. 
+                    With me by your side, you can keep making money while you sleep as your Email marketing effort becomes automated.
+                </p>
+
+                <p>
+                    Contact me today so that we can discuss how to put your Email marketing on autopilot.
+                </p>
+                </div>
+
+            </div>
+
+
+            <div class="service-side">
+                <div>
+                <h3 class="service-title">Social Media Management Services</h3>
+                <img class="service-image" src="/images/Tayo-Adepetu-Social-Media-Marketing.jpg" alt="Tayo-Adepetu-Social--Media-Marketing, image taken from https://www.vecteezy.com/free-vector/social-media">
+                <p class="service-tips">Increase Brand Awareness + Increase Social Engagement + Get More Leads + Increase Conversion Rate
+                </p>
+                </div>
+
+                <div class="service-align">
+                <p>Social Media has become a growth engine that every business needs to get more sales and generate more leads. 
+                    However, the challenge lies in how you will reach the right set of people who can and are willing to pay for your services or products. 
+                    Keep calm! While you face other important aspects of your business, I can help you with your social media management needs.
+                </p>
+
+                <p>I offer social media management services that can help your business build a loyal customer base, increase brand awareness, 
+                    maintain fruitful engagement with your customers, increase social media followers, 
+                    generate more leads, and increase profits through my data-driven, social media management strategies.
+                </p>
+
+                <p>
+                    Are you in search of an effective, reliable Social Media Manager? I am here to help you achieve your social media goals. 
+                    When it comes to managing multiple social media channels for businesses and private individuals, I am one of the best experts you can trust.
+                </p>
+
+                <P>I help develop highly-effective social media strategies for individuals and businesses. 
+                    I have an excellent understanding of what distinguishes effective social media campaigns and strategies from common, basic social media efforts. 
+                    This knowledge, combined with my years of experience and the cutting-edge technologies at my disposal will let me get excellent results for your business.
+                </P>
+                </div>
+            </div>          
+
+        </div>
+
+        <div class="service-section">
+            <div class="service-side">
+                <div>
+                <h3 class="service-title">Blog and Website Management Services</h3>
+                <img class="service-image" src="/images/Tayo-Adepetu-Blog-Management.jpg" alt="Tayo-Adepetu-Blog-Management, image taken from https://www.vecteezy.com/free-vector/blog">
+                <p class="service-tips">Enhance Your Website's On-page, Off-page, and Technical SEO + Publish Quality Blog Posts + Increase Traffic To Your Website + Increase Sales</p>
+
+                </div>
+
+                <div class="service-align">
+                <p>
+                    You may not have the time or skill needed to manage your website and make it successful. It's okay; let not your mind be dismayed. 
+                    Blogging and website management requires time, skill, and consistency. Therefore, you might a blog or website manager to help you. 
+                    I'm a Digital Marketer who runs a full-service digital marketing agency. I have the knowledge, skills and resources you need to take your blog or website to the next level.
+                </p>
+
+                <p>
+                    My aim is to put your mind at rest, so that you can have enough time to face other aspects of your business. Let me help you think of the right content marketing and publishing strategy, 
+                    On and Off-page SEO strategy, content distribution, article structure, audience research, and every other thing that can make you dominate both local and international SEO in your industry. 
+                    You deserve that freedom.
+                </p>
+
+                <p>
+                    I believe the aim of every business engaging in content marketing is to acquire more sales. Therefore, I'm not only promising awesome content and traffic, 
+                    but I'm also promising you increased sales. Contact me so that we can discuss your roadmap to SEO success.
+                </p>
+                </div>
+            </div>
+
+
+            <div class="service-side">
+                <div>
+                <h3 class="service-title">Lead Magnets Writing Services</h3>
+                <img class="service-image" src="/images/Tayo-Adepetu-Lead-Generation-Content-Writing.jpg" alt="Tayo-Adepetu-Lead-Magnets-Creation, image taken from https://www.vecteezy.com/free-vector/lead">
+                <p class="service-tips">Get More Email Subscribers + Generate More Leads + Increase Conversion Rate</p>
+
+                </div>
+
+                <div class="service-align">
+                <p>
+                    Every business that desires to generate leads digitally must offer lead magnets to their audience. 
+                    Most buyers dont buy the first they encounter your offer. In most cases, you have to sensitize them several times on why your offer is the best for them. 
+                    This means there must be a medium of contacting them, ususally via email, even after they must have left your website or social media profiles. 
+                    This is where lead magnets come in. Without lead magnets, it becomes hard for you to get the emails of your target audience.
+                </p>
+
+                <p>
+                    Combining my copywriting with content marketing skills, I can help you come up with lead magnets that are unique from what's already common in your industry.
+                    Your lead magnets must solve real problems for your target audience, so my aim is to give you give lead magnets that are informative, valuable, and helpful to your audience.
+                </p>
+
+                <p>
+                    Be it Whitepaper, E-books, Case Studies, Checklists, Cheatsheets, Templates, Infographics, Research Reports, etc., I can help you develop lead magnets that will 
+                    showcase your thought leadership abilities, make your audience drop their contact details without stress on your part, and increase your lead generation and conversion rates. 
+                </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="testy-call">
+            <div class="testimonial">
+                <div class="testimonial-headline">Testimonials</div>
+                <div class="testimony">
+                    <div>
+                        "Tayo was extremely communicative, creative, and he really has a way with words! 
+                        He drafted captions for my social media posts and authored and designed content for my blog posts. 
+                        I highly recommend! Tayo is a go-to person if you want to get things DONE"
+                    </div>
+                    <div>
+                        <div class="testy-name">Dionne Long</div>
+                        <div class="testy-firm">Founder/CEO of Mantra Consulting, Philadelphia </div>
+                    </div>
+                    <div>
+                        <arrow-left-icon :size="48"/>
+                        <arrow-right-icon :size="48"/>
+                        
+                    </div>
+                </div>
+
+            </div>            
+
+        </div>
+
+        <div class="called">
+                <div class="call-head"> Let’s work together! </div>
+                <div class="calling-long">
+                    Find the right audience, put your mind at rest while I focus on doing the great job for you.
+                </div>
+                <div class="last-call-to-action">Work With Me</div>
+        </div>
+
+        <div class="footer">
+            <div class="footnote"><p>©2022 by TAYO ADEPETU</p></div>
+
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
