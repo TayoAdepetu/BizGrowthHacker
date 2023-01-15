f17635e711bde225ee6471bf8f5df1901d468bd0
TayoAdepetu <copyafrica93@gmail.com>
2023-01-15 03:50:33 -0800

    mobile-friendly

--- resources/js/components/HomePage.vue
+++ resources/js/components/HomePage.vue
@@ -2,27 +2,29 @@
     <div class="page-container">
 
         <div class="top-area">
-            <div class="menu-bar menuu">
-            <div class="page-header">Business Growth Hacker</div>
-            <div class="menu-menu">
-                <div class="service-page">Services</div>
-                <div class="work-page-border">Work With Me</div>
-            </div>
+            <div class="menuuu">
+                <div class="menu-bar">
+                    <div class="page-header">Business Growth Hacker</div>
+                    <div class="menu-menu">
+                        <div class="service-page">Services</div>
+                        <div class="work-page-border">Work With Me</div>
+                    </div>
+                </div>
             </div>
            
             <div class="the-hero">
                 <div class="first-flex">
                     <div class="hit-line">
-                        <div>I Want To Make You Some Money.</div>
+                        <div>I Want To Help You Make More Money.</div>
                         <div>Will You Permit Me?</div>
                     </div>
                     <div class="intro-line">
                         <div class="name-tag">
-                        <span class="second-name">I am Tayo Adepetu</span><span class="first-name">, a Full-service Digital Marketer For Small and Medium-scale Businesses.</span>
+                        <span class="second-name">I am Tayo Adepetu</span><span class="first-name">, a Copywriter and Tech Hobbyist.</span>
                         </div>
                         
-                        <div>My aim is to help small and medium-scale businesses (just a few at a time) make more money by increasing their brand awareness, enhancing their online visibility, and communicating their offers clearly and persuasively.</div>
-                        <div class="brief-service">Copywriting, SEO, Socials, Emails, Branding, and Advertising.</div>
+                        <div>My aim is to help businesses make more money through quality content that enhances brand awareness and online visibility, and communicates their offers clearly and persuasively.</div>
+                        <div class="brief-service">Copywriting - SEO and Thought Leadership Content - Emails and Socials.</div>
                         <div class="call-to-action">Work With Me</div>
                     </div>  
                 </div>          
@@ -36,7 +38,7 @@
         </div>
 
         <div class="service-opening">
-            <div class="money-hit-line">How Will I Help You Make <span class="more-money">More Money</span>?</div>
+            <div class="money-hit-line">How Will I Help You Make <div class="more-money">More Money?</div></div>
             <div class="money-intro">
                 I believe the process of making money starts from adding value. 
                 Once you add value to your business and your target audience, it's only natural for your revenues to grow. 
@@ -47,7 +49,7 @@
         <div class="service-section">
             <div class="service-side">
                 <div>
-                <h3 class="service-title">Copywriting and Copy Optimization Services</h3>
+                <h3 class="service-title">Copywriting Services</h3>
                 <img class="service-image" src="/images/Tayo-Adepetu-Copywriting.jpg" alt="Tayo-Adepetu-Copywriting, image taken from https://www.vecteezy.com/free-vector/copywriting">
                 
                 <p class="service-tips">Generate More Leads + Increase Sales + Make More Money</p>
@@ -55,8 +57,8 @@
 
                 <div class="service-align">
                     <p>
-                        Is your website not bringing enough sales or clients? Are your social media accounts not contributing enough to your revenues? 
-                        Are your ad copies disappoI Have The Solutions You Seek.
+                        Is your website not bringing enough sales? Are your social media accounts not contributing enough to your revenues? 
+                        Are your ad copies disappointing? I Have The Solutions You Seek.
                     </p>
 
                     <p>
@@ -66,7 +68,7 @@
                     </p>
 
                     <p>
-                        Let me tell your brand’s story and share your business’ values with the world. 
+                        Let me tell your brand’s story and share your business’ values to the world. 
                         And lots of magic happens when I do that – enhanced brand awareness, increased engagement, brand consistency, more leads, more sales, and higher ROI.
                     </p>
 
@@ -83,7 +85,7 @@
 
             <div class="service-side">
                 <div>
-                <h3 class="service-title">SEO and Content Marketing Services</h3>
+                <h3 class="service-title">SEO and Thought Leadership Content Writing Services</h3>
                 <img class="service-image" src="/images/Tayo-Adepetu-SEO-Writing.jpg" alt="Tayo-Adepetu-SEO-Writing, image taken from https://www.vecteezy.com/free-vector/seo">
                 <p class="service-tips">Get Quality Content That Adds Value to Your Readers + Rank High in Search Engines</p>
                 </div>
@@ -100,13 +102,13 @@
                 </P>
 
                 <p>
-                    From landing pages to company bios, product/category descriptions, adverts, headlines, newsletters, and other SEO and promotional contents, 
+                    From blog articles to public relations content and other SEO and promotional contents, 
                     content marketing strategies must be laser-focused on customer needs. Indeed, when it comes to content marketing and search engine optimization, 
                     there's no substitute for quality content.
                 </p>
 
                 <p>
-                    With me on your side, your readers will keep coming back to read your articles. The more they read your articles, the more money you make.
+                    With me by your side, your readers will keep coming back to read your articles. And I will ensure that the more they read your articles, the more money you make.
                 </p>
                 </div>
             </div>
@@ -116,126 +118,72 @@
         <div class="service-section">
             <div class="service-side">
                 <div>
-                <h3 class="service-title">Email Marketing and Automation Services</h3>
+                <h3 class="service-title">Email and Lead Magnet Writing Services</h3>
                 <img class="service-image" src="/images/Tayo-Adepetu-Email-Marketing.jpg" alt="Tayo-Adepetu-Email-Marketing, image taken from https://www.vecteezy.com/free-vector/email-marketing">
-                <p class="service-tips">Reach New Audiences + Generate Leads + Get New Customers + Convince Old Customers To Buy and Turn Them To Your Brand's Advocates</p>
-
-                </div>
-
-                <div class="service-align">
-                <p>
-                    No doubt, Email marketing is an effective marketing strategy. In fact, it's one of the most effective marketing strategies out there. 
-                    However, for it to be so effective, you need to have a solid plan. 
-                    To have a solid Email marketing plan, you need to know your onions. Now, you don't have to take a course on Email marketing. I've done that for you.
-                     Just face other areas of your business and enjoy more time with your family. Being a Digital Marketer, Email marketing is my business.
-                </p>
-
-                <p>I can help you find the right audience, collect their data, segment the audience according to their buying journeys, and target each segment with relevant, 
-                    contextual emails that can add value to them and nurture them until they're able and ready to buy.
-                </p>  
-
-                <p>I have the tools and skills to help you generate new leads and convince both your new and existing customers to buy more from you. 
-                    With me by your side, you can keep making money while you sleep as your Email marketing effort becomes automated.
-                </p>
-
-                <p>
-                    Contact me today so that we can discuss how to put your Email marketing on autopilot.
-                </p>
-                </div>
-
-            </div>
-
-
-            <div class="service-side">
-                <div>
-                <h3 class="service-title">Social Media Management Services</h3>
-                <img class="service-image" src="/images/Tayo-Adepetu-Social-media-marketing.jpg" alt="Tayo-Adepetu-Social--Media-Marketing, image taken from https://www.vecteezy.com/free-vector/social-media">
-                <p class="service-tips">Increase Brand Awareness + Increase Social Engagement + Get More Leads + Increase Conversion Rate
-                </p>
-                </div>
-
-                <div class="service-align">
-                <p>Social Media has become a growth engine that every business needs to get more sales and generate more leads. 
-                    However, the challenge lies in how you will reach the right set of people who can and are willing to pay for your services or products. 
-                    Keep calm! While you face other important aspects of your business, I can help you with your social media management needs.
-                </p>
-
-                <p>I offer social media management services that can help your business build a loyal customer base, increase brand awareness, 
-                    maintain fruitful engagement with your customers, increase social media followers, 
-                    generate more leads, and increase profits through my data-driven, social media management strategies.
-                </p>
-
-                <p>
-                    Are you in search of an effective, reliable Social Media Manager? I am here to help you achieve your social media goals. 
-                    When it comes to managing multiple social media channels for businesses and private individuals, I am one of the best experts you can trust.
-                </p>
-
-                <P>I help develop highly-effective social media strategies for individuals and businesses. 
-                    I have an excellent understanding of what distinguishes effective social media campaigns and strategies from common, basic social media efforts. 
-                    This knowledge, combined with my years of experience and the cutting-edge technologies at my disposal will let me get excellent results for your business.
-                </P>
-                </div>
-            </div>          
-
-        </div>
-
-        <div class="service-section">
-            <div class="service-side">
-                <div>
-                <h3 class="service-title">Blog and Website Management Services</h3>
-                <img class="service-image" src="/images/Tayo-Adepetu-Blog-Management.jpg" alt="Tayo-Adepetu-Blog-Management, image taken from https://www.vecteezy.com/free-vector/blog">
-                <p class="service-tips">Enhance Your Website's On-page, Off-page, and Technical SEO + Publish Quality Blog Posts + Increase Traffic To Your Website + Increase Sales</p>
-
-                </div>
-
-                <div class="service-align">
-                <p>
-                    You may not have the time or skill needed to manage your website and make it successful. It's okay; let not your mind be dismayed. 
-                    Blogging and website management requires time, skill, and consistency. Therefore, you might a blog or website manager to help you. 
-                    I'm a Digital Marketer who runs a full-service digital marketing agency. I have the knowledge, skills and resources you need to take your blog or website to the next level.
-                </p>
-
-                <p>
-                    My aim is to put your mind at rest, so that you can have enough time to face other aspects of your business. Let me help you think of the right content marketing and publishing strategy, 
-                    On and Off-page SEO strategy, content distribution, article structure, audience research, and every other thing that can make you dominate both local and international SEO in your industry. 
-                    You deserve that freedom.
-                </p>
-
-                <p>
-                    I believe the aim of every business engaging in content marketing is to acquire more sales. Therefore, I'm not only promising awesome content and traffic, 
-                    but I'm also promising you increased sales. Contact me so that we can discuss your roadmap to SEO success.
-                </p>
-                </div>
-            </div>
-
-
-            <div class="service-side">
-                <div>
-                <h3 class="service-title">Lead Magnets Writing Services</h3>
-                <img class="service-image" src="/images/Tayo-Adepetu-Lead-Generation-Content-Writing.jpg" alt="Tayo-Adepetu-Lead-Magnets-Creation, image taken from https://www.vecteezy.com/free-vector/lead">
-                <p class="service-tips">Get More Email Subscribers + Generate More Leads + Increase Conversion Rate</p>
+                <p class="service-tips">Reach New Audiences + Generate Leads + Get New Customers + Convince Old Customers To Buy More and Turn Them To Your Brand's Advocates</p>
 
                 </div>
 
                 <div class="service-align">
                 <p>
                     Every business that desires to generate leads digitally must offer lead magnets to their audience. 
-                    Most buyers dont buy the first they encounter your offer. In most cases, you have to sensitize them several times on why your offer is the best for them. 
+                    Most buyers don't buy the first time they encounter your offer. In most cases, you have to sensitize them several times on why your offer is the best for them. 
                     This means there must be a medium of contacting them, ususally via email, even after they must have left your website or social media profiles. 
                     This is where lead magnets come in. Without lead magnets, it becomes hard for you to get the emails of your target audience.
                 </p>
 
                 <p>
-                    Combining my copywriting with content marketing skills, I can help you come up with lead magnets that are unique from what's already common in your industry.
-                    Your lead magnets must solve real problems for your target audience, so my aim is to give you give lead magnets that are informative, valuable, and helpful to your audience.
+                    By combining my excellent copywriting and researching skills, I can help you come up with lead magnets that are unique from what's already common in your industry.
+                    Your lead magnets must solve real problems for your target audience, so my aim is to give you lead magnets that are informative, valuable, and helpful to your audience.
                 </p>
 
+                <p>Be it Whitepaper, E-books, Case Studies, Checklists, Cheatsheets, Templates, Infographics, Research Reports, etc., I can help you develop lead magnets that will 
+                    showcase your thought leadership abilities, make your audience drop their contact details without stress on your part, and increase your lead generation and conversion rates. </p>
+
                 <p>
-                    Be it Whitepaper, E-books, Case Studies, Checklists, Cheatsheets, Templates, Infographics, Research Reports, etc., I can help you develop lead magnets that will 
-                    showcase your thought leadership abilities, make your audience drop their contact details without stress on your part, and increase your lead generation and conversion rates. 
+                    After getting their email addresses, I can help you write the right email content that will target them according to their buying journeys, 
+                    add value to them, and nurture them until they're able and ready to buy.                    
                 </p>
                 </div>
             </div>
+
+
+            <div class="service-side">
+                <div>
+                <h3 class="service-title">Social Media Content Writing Services</h3>
+                <img class="service-image" src="/images/Tayo-Adepetu-Lead-Generation-Content-Writing.jpg" alt="Tayo-Adepetu-Lead-Magnets-Creation, image taken from https://www.vecteezy.com/free-vector/lead">
+                <p class="service-tips">Increase Brand Awareness + Increase Social Engagement + Get More Leads + Increase Conversion Rate
+                </p>
+                </div>
+
+                <div class="service-align">
+                <p>Social Media has become a growth engine that every business needs to generate more leads and get more sales. 
+                    However, the challenge lies in how you will let the right content reach the right set of people 
+                    who can and are willing to pay for your services or products. 
+                </p>
+                <p>
+                    Keep calm! While you face other important aspects of your business, 
+                    I can work with your social media manager to help create the right social media content
+                    that talks directly to your real audience.
+                </p>
+
+                <p>I offer social media content writing services that can help your business build a loyal customer base, 
+                    increase brand awareness, 
+                    maintain fruitful engagement with your customers, increase social media followers, 
+                    generate more leads, and increase profits.
+                </p>
+
+                <p>
+                    Are you in search of an effective, reliable Social Media Content Writer? 
+                    I am here to help you achieve your social media goals. 
+                </p>
+
+                <P>Be it short captions for Instagram and Twitter or long-form content for LinkedIn and Facebook, 
+                    I can give you content that will get you excellent results.
+                </P>
+                </div>
+            </div>          
+
         </div>
 
         <div class="testy-call">
@@ -271,7 +219,7 @@
         </div>
 
         <div class="footer">
-            <div class="footnote"><p>©2022 by TAYO ADEPETU</p></div>
+            <div class="footnote"><p>©2022 Website Coded by TAYO ADEPETU Using Laravel and VueJS</p></div>
 
         </div>
 
